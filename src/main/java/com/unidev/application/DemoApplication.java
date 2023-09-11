dc541892a6684a0da9088110a9180e5d85da63fc
Odie100 <andriampeno.odilon@gmail.com>
2023-09-11 10:08:16 +0300

    refactor: optimize import

--- src/main/java/com/unidev/application/DemoApplication.java
+++ src/main/java/com/unidev/application/DemoApplication.java
@@ -2,9 +2,6 @@ package com.unidev.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.w3c.dom.stylesheets.LinkStyle;
-
-import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
