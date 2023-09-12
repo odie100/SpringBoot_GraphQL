dc8c812acd418fc4ae8e6af8a53f9bb54b753a68
Odie100 <andriampeno.odilon@gmail.com>
2023-09-12 10:22:15 +0300

    feat: find Wallet by id feat: new graphql api for wallet by id

--- src/main/java/com/unidev/application/api/WalletGraphQLAPI.java
+++ src/main/java/com/unidev/application/api/WalletGraphQLAPI.java
@@ -3,6 +3,7 @@ package com.unidev.application.api;
 import com.unidev.application.entities.Wallet;
 import com.unidev.application.repositories.WalletRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -16,5 +17,10 @@ public class WalletGraphQLAPI {
     @QueryMapping
     private List<Wallet> userWallets(){
         return walletRepository.findAll();
+    }
+
+    @QueryMapping
+    private Wallet walletById(@Argument String id){
+        return this.walletRepository.findById(id).orElseThrow(()-> new RuntimeException("Wallet not found"));
     }
 }
